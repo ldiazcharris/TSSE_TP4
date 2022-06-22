bfe3255d527b8706db5159477c8b9629a9009ec4
ldiazcharris <lddiazcharris@gmail.com>
2022-06-22 09:33:05 -0500

    creacion proyecto ceedling

---
+++ tp4/build/test/cache/test_tp4.c
@@ -0,0 +1,10 @@
+#include "/var/lib/gems/2.7.0/gems/ceedling-0.31.1/vendor/unity/src/unity.h"
+
+
+void test_prueba1()
+
+{
+
+    UnityFail( (("EMPEZAMOS")), (UNITY_UINT)(5));
+
+}
---
+++ tp4/test/test_tp4.c
@@ -0,0 +1,27 @@
+/**
+ * @file test_tp4.c
+ * @author Luis David Diaz Charris (lddiazcharris@gmail.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-06-22
+ * 
+ * @copyright Creative Commons 2022
+ * 
+ */
+
+#include "unity.h"
+
+
+/** -------------------PRUEABAS A REALIZAR----------------------------------
+ *      1. 
+ *      2.
+ *      3. 
+ *      4. 
+ *      5. 
+ *  
+ */
+
+void test_prueba1()
+{
+    TEST_FAIL_MESSAGE("EMPEZAMOS");
+}
